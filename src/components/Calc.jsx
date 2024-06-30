9169aede52268dcc8b0b446d7351cd4a6837a487
AtanasovCode <a_ljupce@yahoo.com>
2024-06-30 15:22:55 +0200

    installed tailwindcss

---
+++ src/components/Calc.jsx
@@ -0,0 +1,13 @@
+
+
+const Calc = () => {
+    return (
+        <div className="bg-slate-800 items-center justify-center p-4">
+            <div className="font-bold text-white text-2xl text-center">
+                Calculator.js
+            </div>
+        </div>
+    );
+}
+
+export default Calc;
